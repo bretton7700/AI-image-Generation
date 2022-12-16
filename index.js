47012d0955f0a3396e8cd5921f1de16243713456
gitonga bretton <gitongabretton@gmail.com>
2022-12-17 01:17:40 +0300

    openai router

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use('/ai',require('./routes/openaiRoutes'));
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
