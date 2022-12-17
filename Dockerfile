a2196aa5a4aca9e39e15c07a5016545edbdf6879
gitonga bretton <gitongabretton@gmail.com>
2022-12-17 03:38:31 +0300

    complete code

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+#RUN rc-update add docker boot
+FROM ubuntu:latest
+CMD ['bash']
+FROM node:14.18.1
+#app directory
+WORKDIR /usr/src/app
+#install app dependencies
+COPY  package*.json ./
+
+#for production
+RUN npm ci --only=production
+
+#bundle app source
+COPY . .
+
+EXPOSE 5000
+
+CMD ['bash']
+#SHELL ["/bin/bash", "-c"]
+ENTRYPOINT  ["node", "index.js"]
---
+++ public/js/main.js
@@ -0,0 +1,57 @@
+function onSubmit(e) {
+    e.preventDefault();
+  
+    document.querySelector('.msg').textContent = '';
+    document.querySelector('#image').src = '';
+  
+    const prompt = document.querySelector('#prompt').value;
+    const size = document.querySelector('#size').value;
+  
+    if (prompt === '') {
+      alert('Please add some text');
+      return;
+    }
+  
+    generateImageRequest(prompt, size);
+  }
+  
+  async function generateImageRequest(prompt, size) {
+    try {
+      showSpinner();
+  
+      const response = await fetch('/ai/generateimage', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          prompt,
+          size,
+        }),
+      });
+  
+      if (!response.ok) {
+        removeSpinner();
+        throw new Error('That image could not be generated');
+      }
+  
+      const generatedImageData = await response.json();
+      const imageUrl = generatedImageData.data;
+      document.querySelector('#image').src = imageUrl;
+  
+        
+      removeSpinner();
+    } catch (error) {
+      document.querySelector('.msg').textContent = error;
+    }
+  }
+  
+  function showSpinner() {
+    document.querySelector('.spinner').classList.add('show');
+  }
+  
+  function removeSpinner() {
+    document.querySelector('.spinner').classList.remove('show');
+  }
+  
+  document.querySelector('#image-form').addEventListener('submit', onSubmit);
